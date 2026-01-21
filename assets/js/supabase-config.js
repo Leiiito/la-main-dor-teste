af25e76710904546a632bb36c969d77137ad1e3d
Boutique <behramanon@gmail.com>
2026-01-21 02:55:27 +0100

    Update supabase-config.js

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,7 +1,7 @@
 export const SUPABASE_URL = "https://oyewvpbqtzdcqbyomsu.supabase.co";
 
 // ⚠️ ICI : clé anon LEGACY (JWT)
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95ZXd2cGJxdHpkY3FieXBvbXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5NDMxMTAsImV4cCI6MjA4NDUxOTExMH0.WPDn3G7OKPXUI8Y15goe_c0FFovE7VID7VvT_6-hNAI";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im95ZXd2cGJxdHpkY3FieXBvbXN1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODk0MzExMCwiZXhwIjoyMDg0NTE5MTEwfQ.e1rpWkur2V82SrQPsh2GFwpUFVNEU-9yYatQaE2R41k";
 
 export const EDGE_FUNCTION_NAME = "admin-api";
 export const IMAGES_BUCKET = "public-images";
